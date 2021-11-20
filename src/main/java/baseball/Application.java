5a3cc03e37fd417258892e3be70a37eb3ce26655
is2js <tingstyle1@gmail.com>
2021-11-20 17:36:09 +0900

    feat: class 변수, 상수까지 설명완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,9 +9,6 @@ public class Application {
         Calculator teacher = new Calculator();
         System.out.println(teacher.add(1000, 2000));
 
-        //System.out.println(teacher.shareResult);
-        // 클래스 변수는 바로 클래스에서 접근할 수 있다.
-        // 굳이 모든 인스턴스 공통인데 한 인스턴스에서 접근 ㄴㄴ
         System.out.println(Calculator.shareResult);
     }
 }
