8c77c977a60bce1965fe7fa058d0f7160536d075
is2js <tingstyle1@gmail.com>
2021-11-21 17:24:23 +0900

    my) 클래스1 메소드1에 추가 경우마다 @Test 아래 추가 생성

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -9,24 +9,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RefereeTest {
-    //1. comppare( , )함수의 결과를 테스트하기 위해
-    // -> 인스턴스를 통해 메서드기능 실행시키는 것은 똑가다.
-    // -> 결과값까지 받아두고.. 처리하는데..
     @Test
     void 스트라이크3() {
-        // 1. 클래스 사용ㅇ하는 것은  Test 아닐때 -> Application matn메소드에서 하는 것 과 비슷하다.
         Referee referee = new Referee();
-        // 일단, 알고 있는 답이 나오도록 파라미터들을 넣어준다.
         String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
-        //2. "0 볼 3 스틀라이크"의 확인을 컴퓨터에게 맞겨야한다. -> printlin은 그만!
-        //System.out.println(result);
-        
-        // 3. assertj라는 라이브러리를 이용해서 
-        // ctlr+. -> import static method -> Assertions.assertThat 선택
-        // -> 내가 알고 있는 답은 "이거야" 라고 주장(assert)한다.
-        // -> assertThat( 결과변수 ) -> 이 결과변수는
-        // -> .isEualTo("값") -> 이 값과 같다고 나는 주장한다.
-        // -> print안찍을테니까 너가 확인해줘.
         assertThat(result).isEqualTo("0 볼 3 스트라이크");
+
+    //1. 이번에는 다른 파라미터 -> 다른 아는 답이 나오는 경우도 테스트해본다.
+    // -> my) 그냥 1메소드마다 여러경우를  main에서 하는 것처럼 테스트하는거네..
+    // **위에 1번째 경우 코드를 복사해서 파라미터 & 주장하는답(expected)만 수정한다.**
     }
+    @Test
+    void 아웃() {
+        Referee referee = new Referee();
+        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(7, 8, 9));
+        assertThat(result).isEqualTo("낫싱");
+    }
+
+    //2. 다른 아는 파라미터 + 아는 다른 답을 넣어준다.
+    @Test
+    void 볼3() {
+        Referee referee = new Referee();
+        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(3, 1, 2));
+        assertThat(result).isEqualTo("3 볼 0 스트라이크");
+    }
+    // 3. 하나만 더 테스트
+    @Test
+    void 볼2_스트라이크1() {
+        Referee referee = new Referee();
+        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(3, 2, 1));
+        assertThat(result).isEqualTo("2 볼 1 스트라이크");
+    }
+
 }
